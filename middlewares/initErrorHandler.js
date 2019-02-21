1554ae44a93aabca2301a7137f0adfdecc9b1c29
Oleg Miniuk <o.minyuk@itransition.com>
2019-02-21 20:20:50 +0300

    nodemon added & eslint fixes

--- middlewares/initErrorHandler.js
+++ middlewares/initErrorHandler.js
@@ -1,12 +1,14 @@
-const initErrorHandler = app => {
+/* eslint-disable no-unused-vars */
+
+const initErrorHandler = (app) => {
   // catch 404 and forward to error handler
-  app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+  app.use((req, res, next) => {
+    const err = new Error('Not Found');
     err.status = 404;
     next(err);
   });
 
-  app.use(function(err, req, res, next) {
+  app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
